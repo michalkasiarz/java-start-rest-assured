552f6cb3afe39cda0e74916417d417dbf0346257
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-09-20 14:07:43 +0200

    User class added, UserCreationTests refactored

---
+++ src/main/java/pl/javastart/main/pojo/User.java
@@ -0,0 +1,77 @@
+package pl.javastart.main.pojo;
+
+public class User {
+
+    private String id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String userStatus;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+}
--- src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -1,6 +1,7 @@
 package pl.javastart.restassured.test.tasks;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.User;
 
 import static io.restassured.RestAssured.given;
 
@@ -11,28 +12,26 @@ public class UserCreationTests {
     @Test
     public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
 
-        String user = "{\n" +
-                "  \"id\": 445,\n" +
-                "  \"username\": \"firstuser\",\n" +
-                "  \"firstName\": \"Krzysztof\",\n" +
-                "  \"lastName\": \"Kowalski\",\n" +
-                "  \"email\": \"krzysztof@test.com\",\n" +
-                "  \"password\": \"password\",\n" +
-                "  \"phone\": \"+123456789\",\n" +
-                "  \"userStatus\": 1\n" +
-                "}";
+        User user1 = new User();
+        user1.setId("445");
+        user1.setUserName("firstuser");
+        user1.setFirstName("Krzysztof");
+        user1.setLastName("Kowalski");
+        user1.setEmail("krzysztof@test.com");
+        user1.setPassword("password");
+        user1.setPhone("+123456789");
+        user1.setUserStatus("1");
 
-        given().log().uri().log().method().body(user)
+        given().log().uri().log().all()
                 .contentType("application/json")
+                .body(user1)
                 .when().post(baseURL)
                 .then().log().everything().statusCode(200);
 
-        given().log().method().log().uri()
+        given().log().all()
                 .contentType("application/json")
-                .pathParam("username", "firstuser")
+                .pathParam("username", user1.getUserName())
                 .when().get(baseURL + "/{username}")
                 .then().log().all().statusCode(200);
-
     }
-
 }
