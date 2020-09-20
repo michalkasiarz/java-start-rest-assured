6f235a0b7474eeab1a9157cccfc66ddd853ac1fc
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-09-20 17:44:44 +0200

    deserialization test with assertions, yet another example

--- src/test/java/pl/javastart/restassured/test/serialization/SerializationAndDeserializationTests.java
+++ src/test/java/pl/javastart/restassured/test/serialization/SerializationAndDeserializationTests.java
@@ -45,4 +45,44 @@ public class SerializationAndDeserializationTests {
         assertEquals(actualPet.getStatus(), pet.getStatus(), "Pet status");
     }
 
+    @Test
+    public void givenExistingPetIdWhenGetPetThenReturnPetTest() {
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs");
+
+        Pet pet = new Pet();
+        pet.setId(123);
+        pet.setCategory(category);
+        pet.setPhotoUrls(Collections.singletonList("http://photos.com/dog1.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
+
+
+        Pet postedPet = given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200)
+                .extract().as(Pet.class);
+
+
+        Pet actualPet = given().log().uri().log().method()
+                .pathParam("petId", postedPet.getId())
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().log().all().statusCode(200)
+                .extract().as(Pet.class);
+
+        assertEquals(actualPet.getId(), pet.getId(), "Pet id");
+        assertEquals(actualPet.getCategory().getId(), pet.getCategory().getId(), "Category id");
+        assertEquals(actualPet.getCategory().getName(), pet.getCategory().getName(), "Category name");
+        assertEquals(actualPet.getPhotoUrls().get(0), pet.getPhotoUrls().get(0), "Photo URL");
+        assertEquals(actualPet.getTags().get(0).getId(), pet.getTags().get(0).getId(), "Pet tag id");
+        assertEquals(actualPet.getTags().get(0).getName(), pet.getTags().get(0).getName(), "Pet tag name");
+        assertEquals(actualPet.getStatus(), pet.getStatus(), "Pet status");
+    }
+
 }
