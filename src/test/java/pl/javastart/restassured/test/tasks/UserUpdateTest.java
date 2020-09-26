54ec54e8fc286736023b0b11bfe10a03e5890e53
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-09-26 08:52:38 +0200

    requestSpecBuilder introduced

--- src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTest.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTest.java
@@ -1,14 +1,23 @@
 package pl.javastart.restassured.test.tasks;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.Test;
 import pl.javastart.main.pojo.User;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UserUpdateTest extends BaseTest {
 
     @Test
     public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setContentType("application/json");
+        RequestSpecification defaultRequestSpecification = requestSpecBuilder.build();
 
         User user = new User();
         user.setId("445");
@@ -20,27 +29,45 @@ public class UserUpdateTest extends BaseTest {
         user.setPhone("+123456789");
         user.setUserStatus("1");
 
-        given()
-                .contentType("application/json")
+        ResponseSpecBuilder postResponseSpecBuilder = new ResponseSpecBuilder();
+        postResponseSpecBuilder
+                .expectBody("code", equalTo(200))
+                .expectBody("type", equalTo("unknown"))
+                .expectBody("message", equalTo("445"))
+                .expectStatusCode(200);
+        ResponseSpecification userCreationResponseSpecification = postResponseSpecBuilder.build();
+
+        given().spec(defaultRequestSpecification)
                 .body(user)
                 .when().post("user")
-                .then().statusCode(200);
+                .then().assertThat().spec(userCreationResponseSpecification);
 
         user.setFirstName("Janusz");
         user.setLastName("Nowak");
 
-        given()
-                .contentType("application/json")
+        given().spec(defaultRequestSpecification)
                 .pathParam("username", user.getUserName())
                 .body(user)
                 .when().put("user/{username}")
-                .then().statusCode(200);
+                .then().assertThat().spec(userCreationResponseSpecification);
 
-        given()
-                .contentType("application/json")
+        ResponseSpecBuilder updateResponseSpecBuilder = new ResponseSpecBuilder();
+        updateResponseSpecBuilder
+                .expectBody("id", equalTo("445"))
+                .expectBody("username", equalTo("firstuser"))
+                .expectBody("firstName", equalTo("Janusz"))
+                .expectBody("lastName", equalTo("Nowak"))
+                .expectBody("email", equalTo("krzysztof@test.com"))
+                .expectBody("password", equalTo("password"))
+                .expectBody("phone", equalTo("+123456789"))
+                .expectBody("userStatus", equalTo("1"))
+                .expectStatusCode(200);
+        ResponseSpecification getResponseSpecification = updateResponseSpecBuilder.build();
+
+        given().spec(defaultRequestSpecification)
                 .pathParam("username", user.getUserName())
                 .when().get("user/{username}")
-                .then().statusCode(200);
-
+                .then().assertThat().spec(getResponseSpecification);
     }
 }
+
